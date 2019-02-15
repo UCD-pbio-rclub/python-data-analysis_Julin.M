24d0c427c7155905b9bd7e229a6d3f63b8b6ef54
Julin Maloof <jnmaloof@ucdavis.edu>
2019-02-15 09:17:27 -0800

    start problems

--- Chapter8Problemset1.py
+++ Chapter8Problemset1.py
@@ -60,3 +60,35 @@ flight_weather_air.shape
 
 flight_weather_air.head()
 
+# Min Yao
+
+""". Using the same data from last week. (Import my RNA-Seq CPM data from 'Expression Browser_CPM_practice.xlsx' file. Expression Browser_CPM_practice.xlsx) In column labels, the first number means plant genotype and the second part is one letter and a number which means the treatment conditions and sample numbers. please change them to multilevel hierarchical columns labels with column name in front of the whole data. For example, '6_c1' change it to genotype = '6', treatments = 'c', sample_number = '1'.
+2. In row labels, these numbers are Solyc ID for tomatoes. "Solyc" is the 5 letter abbreviation of Solanum lycopersicum, the 2 digit number following the 'Solyc' denotes the chromosome, 'g' denotes that the sequence is a gene, and the 6 digit number following the 'g' identifies the gene on the chromosome. The '.1' denotes the annotation version number of the locus. Please use hierarchical indexing to label another level of index to show which chromosome each gene on and give the index name 'chromosome'.
+3. In order to compare genotypes and treatments, you want to know the summary statistics of different genotypes and treatments. Please calculate the average expression level of each gene in different genotypes and treatments.
+"""
+
+cpm = pd.read_excel('https://github.com/UCD-pbio-rclub/python-data-analysis_MinYaoJ/raw/master/Expression%20Browser_CPM_practice.xlsx')
+
+cpm.head()
+
+cpm = cpm.set_index('Name')
+
+cpm.head()
+
+newcols = pd.DataFrame(cpm.columns)
+
+newcols.head()
+
+newcols = newcols[0].str.split("_", expand=True)
+
+newcols.head()
+
+newcols = pd.concat([newcols[0],newcols[1].str[0], newcols[1].str[1]], axis=1)
+
+newcols.columns = ['genotype', 'treatment', 'sample_number']
+
+newcols.head()
+
+cpm.columns = pd.MultiIndex(newcols)
+
+cpm.head()
